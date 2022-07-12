22ce29d4192edef4d790fcde4eacbb78d3ab6449
dci-student <alinacuznetov@gmail.com>
2022-07-12 13:37:07 +0200

    repetition

---
+++ solution.js
@@ -0,0 +1,39 @@
+/*
+1. Run Along
+
+Create a while loop that runs as long as the variable i is less than 15. Print the value of i.
+*/
+let i = 13
+
+do {
+    console.log(i);
+}
+while (i < 15);
+
+
+/*
+2. Add it up
+
+Create a while loop that calculates the sum of numbers from 1-20.
+*/
+var i = 1
+var sum = 0  
+
+while (i<=20) { 
+    console.log (sum = sum +1)
+}
+
+
+
+/*
+3. Do.. while statement...
+
+Use a do..while loop that executes the statement The number is [i] while i is less than 20.
+*/
+
+var i = 1;
+
+do {
+    console.log("The number is ", i);
+}
+while (i < 20);
